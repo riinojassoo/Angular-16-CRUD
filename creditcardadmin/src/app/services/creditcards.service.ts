774207cce82256a51e20fbb3e5627c8b64e3f176
riinojassoo <riin.ojassoo@hotmail.com>
2024-05-25 00:59:01 +0300

    Web API Tutorial Part #6 - Service Implementation

--- creditcardadmin/src/app/services/creditcards.service.ts
+++ creditcardadmin/src/app/services/creditcards.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CreditCard } from '../models/credit-card';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreditcardsService {
 
-  constructor() { }
+  private apiUrl ="http://localhost:3000/creditcards";
+
+  constructor(private httpClient: HttpClient) { }
+
+  createCreditCard(creditCard: CreditCard): Observable<CreditCard> {
+    return this.httpClient.post<CreditCard>(this.apiUrl, creditCard);
+  }
+
+  getCreditCards(): Observable<CreditCard[]>{
+    return this.httpClient.get<CreditCard[]>(this.apiUrl);
+  }
+
+  getCreditCardById(id: Number): Observable<CreditCard> {
+    const url = '${this.apiUrl}/${id}';
+    return this.httpClient.get<CreditCard>(url);
+  }
+
+  updateCreditCard(creditCard:CreditCard): Observable<CreditCard> {
+    const url = '${this.apiUrl}/${creditCard.id}';
+    return this.httpClient.put<CreditCard>(url, creditCard);
+  }
+
+  deleteCreditCard(id: Number): Observable<void> {
+    const url = '${this.apiUrl}/${id}';
+    return this.httpClient.delete<void>(url);
+  }
 }
