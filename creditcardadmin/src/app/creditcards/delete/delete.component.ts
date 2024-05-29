aec42e0a031f47e085f8f7b3372a630b0b26d5a1
riinojassoo <riin.ojassoo@hotmail.com>
2024-05-29 22:37:16 +0300

    Web API Tutorial Part #9 - Delete Functionality

--- creditcardadmin/src/app/creditcards/delete/delete.component.ts
+++ creditcardadmin/src/app/creditcards/delete/delete.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CreditcardsService } from 'src/app/services/creditcards.service';
 
 @Component({
   selector: 'app-delete',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class DeleteComponent {
 
+  creditCardId!: Number;
+
+  constructor(private router: ActivatedRoute,
+    private route: Router,
+    private creditcardsService: CreditcardsService) {
+    this.creditCardId = parseInt(this.router.snapshot.paramMap.get("id") || '');
+
+    //Delete Functionality
+    this.creditcardsService.deleteCreditCard(this.creditCardId).subscribe(data => {
+      alert("Credit Card Deleted");
+      this.route.navigate(['creditcards']);
+    })
+  }
 }
--- creditcardadmin/src/app/creditcards/view/view.component.ts
+++ creditcardadmin/src/app/creditcards/view/view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CreditCard } from 'src/app/models/credit-card';
 import { CreditcardsService } from 'src/app/services/creditcards.service';
 
@@ -9,9 +10,14 @@ import { CreditcardsService } from 'src/app/services/creditcards.service';
 })
 export class ViewComponent {
   creditCardDetails!: CreditCard;
+  creditCardId!: Number;
 
-  constructor(private creditCardsService: CreditcardsService) {
-    this.creditCardsService.getCreditCardById(3).subscribe((data: CreditCard) => {
+  constructor(private creditCardsService: CreditcardsService,
+    private route: ActivatedRoute) {
+
+    this.creditCardId = parseInt(this.route.snapshot.paramMap.get("id") || '');
+
+    this.creditCardsService.getCreditCardById(this.creditCardId).subscribe((data: CreditCard) => {
       this.creditCardDetails = data;
     })
   }
