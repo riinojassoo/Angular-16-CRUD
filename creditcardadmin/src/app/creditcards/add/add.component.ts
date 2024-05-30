c24452562edf4abc0fc83d9d3f3ce6ca86b74568
riinojassoo <riin.ojassoo@hotmail.com>
2024-05-30 16:20:52 +0300

    Web API Tutorial Part #10.1

--- creditcardadmin/src/app/creditcards/add/add.component.ts
+++ creditcardadmin/src/app/creditcards/add/add.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { CreditCard } from 'src/app/models/credit-card';
+import { CreditcardsService } from 'src/app/services/creditcards.service';
 
 @Component({
   selector: 'app-add',
@@ -7,6 +9,10 @@ import { CreditCard } from 'src/app/models/credit-card';
   styleUrls: ['./add.component.scss']
 })
 export class AddComponent {
+
+  constructor(private CreditcardsService:CreditcardsService,
+    private router: Router){
+  }
 
   newCreditCard: CreditCard = {
     id: undefined,
@@ -25,6 +31,9 @@ export class AddComponent {
 
   saveCreditCard(){
     console.log("Form Submitted");
-    console.log(this.newCreditCard);
+    this.CreditcardsService.createCreditCard(this.newCreditCard).subscribe(data => {
+      alert("Credit Card Added");
+      this.router.navigate(['creditcards']);
+    })
     }
   }
